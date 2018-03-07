1462b4f43f0eba6fca793567a16defaf4ee054da
anthonyrojas <anthonyrojas095@gmail.com>
2018-03-06 17:26:04 -0800

    first commit for client app

---
+++ ClientApp/ClientApp/Models/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.Models
+{
+    class Conversation
+    {
+        public List<User> Participants { get; set;  }
+        public String ChatType { get; set; }
+    }
+}
---
+++ ClientApp/ClientApp/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.Models
+{
+    class Message
+    {
+        public String ID { get; set; }
+        public String Content { get; set; }
+        public User Sender { get; set; }
+        public Conversation MessageConversation { get; set; }
+    }
+}
